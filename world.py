9fba64375857f56d621b64e31378e1af1ff76169
Jason Lunder <jlunder@zagmail.gonzaga.edu>
2021-10-16 14:36:11 -0700

    adding client for display

--- world.py
+++ world.py
@@ -3,12 +3,16 @@ from player import Player
 from direction import Direction
 from body_segment import BodySegment
 import random
+import opc, time
+client = opc.Client('localhost:7890')
 
 class World():
     
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.numLEDs = width*height
+        self.pixels = [(0,0,0)]*numLEDs
         self.board = [[0 for j in range(self.width)] for i in range(self.height)]
         head = (random.randint(0, self.width), random.randint(0, self.height))
         direction = random.choice(list(Direction))
@@ -18,12 +22,19 @@ class World():
 
     def spawn_candy(self, playerBody):
         location = (random.randint(0, self.width), random.randint(0, self.height))
-        while segment.getLocation() in playerBody:
-            location =(random.randint(0, self.width), random.randint(0, self.height))
+        while location in [segment.getLocation() for segment in playerBody]:
+            location = (random.randint(0, self.width), random.randint(0, self.height))
+        self.pixels[location[0]+(self.width*location[1])] = (0, 255, 0)
         return Candy(location)
 
     def update(self):
-        self.player.update()
+        self.player.update(self, self.width)
+        for segment in self.player.getBody():
+            prevLocation = segment.getPrevLocation()
+            location = segment.getLocation()
+            pixels[(prevLocation[1]*width)+prevLocation[0]] = (0, 0, 0)
+            pixels[(location[1]*width)+location[0]] = (255, 0, 0)
+        client.put_pixels(self.pixels)
         if self.player.getHeadLocation() == self.cur_candy.getLocation():
             self.player.eat()
             self.cur_candy.destroy()
