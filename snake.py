ca9eb0e80f765a878fa081e369c56e261584c1a1
Jason Lunder <jlunder@zagmail.gonzaga.edu>
2021-10-16 16:28:30 -0700

    bugfixes part 1000001

--- snake.py
+++ snake.py
@@ -5,30 +5,31 @@ from direction import Direction
 
 def main():
     pygame.init()
+    window = pygame.display.set_mode((300,300))
     clock = pygame.time.Clock()
     world = World(8, 8)
     while world.game_over != True:
-        clock.tick(8)
+        clock.tick(1)
         player = world.getPlayer()
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 d = player.getDirection()
-                if event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                     if d == Direction.UP:
                         player.turnRight()
                     elif d == Direction.DOWN:
                         player.turnLeft()
-                elif event.key == pygame.K_LEFT:
+                elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
                     if d == Direction.DOWN:
                         player.turnRight()
                     elif d == Direction.UP:
                         player.turnLeft()
-                elif event.key == pygame.K_UP:
+                elif event.key == pygame.K_w or event.key == pygame.K_UP:
                     if d == Direction.RIGHT:
                         player.turnLeft()
                     elif d == Direction.LEFT:
                         player.turnRight()
-                elif event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
                     if d == Direction.LEFT:
                         player.turnLeft()
                     elif d == Direction.RIGHT:
